63d13c0b2d54b2817ddca0f594f307c7b1ada377
Balbino Aylagas <balbino.aylagas@telusinternational.com>
2020-12-14 17:22:21 -0600

    feat: add py intro

---
+++ 01-py-intro/good_to_know.py
@@ -0,0 +1,7 @@
+message = """
+Good to know
+    * The most recent major version of Python is Python 3, which we shall be \n\tusing in this tutorial. However, Python 2, although not being updated with anything other than \n\tsecurity updates, is still quite popular.
+    * In this tutorial Python will be written in a text editor. It is possible \n\tto write Python in an Integrated Development Environment, such as Thonny, Pycharm, Netbeans \n\tor Eclipse which are particularly useful when managing larger collections of Python files.
+"""
+
+print(message)
---
+++ 01-py-intro/what_python.py
@@ -0,0 +1,10 @@
+message = """
+What is Python?
+Python is a popular programming language. \nIt was created by Guido van Rossum, \nand released in 1991.
+It is used for:
+    * web development (server-side),
+    * software development,
+    * mathematics,
+    * system scripting.
+"""
+print(message)
